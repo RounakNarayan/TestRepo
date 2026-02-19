d6deebc412796fce78678b8872f69ad7e63b2913
RounakNarayan <narayanrounak9204@gmail.com>
2026-02-19 09:44:46 +0530

    Add files via upload

---
+++ Program_1.cpp
@@ -0,0 +1,14 @@
+// Introduction Code
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double price;
+    cout << "Enter Price" << endl;
+    cin >> price;
+    int roundPrice = price;
+    cout << "Price = " << price << " ~ " << roundPrice << endl;
+    return 0;
+}
